c0aa1ea2fc2fc9e0fd7e3ff167d6bc001a4d1e72
Andrew Joseph <andrew@aljweb.com>
2013-02-02 05:47:34 +0000

    my file

---
+++ ofus.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+
+from Adafruit_CharLCD import Adafruit_CharLCD
+from subprocess import * 
+from time import sleep, strftime
+from datetime import datetime
+
+lcd = Adafruit_CharLCD()
+
+cmd = "ip addr show eth0 | grep inet | awk '{print $2}' | cut -d/ -f1"
+
+lcd.begin(16,1)
+
+def run_cmd(cmd):
+    p = Popen(cmd, shell=True, stdout=PIPE)
+    output = p.communicate()[0]
+    return output
+
+while 1:
+    lcd.clear()
+#    ipaddr = run_cmd(cmd)
+    lcd.message(datetime.now().strftime('%b %d  %H:%M:%S\n'))
+#    lcd.message('IP %s' % ( ipaddr ) )
+#    sleep(2)
+    lcd.message('Ofus is teh win!')
+    sleep(1)
+    
+print "BYE"
+#GPIO.cleanup() 
+#
+#
+#try:
+#    while 1:
+#        print "BEEP"
+#        sleep(1)
+#except KeyboardInterrupt:
+#            print "BYE BYE"
